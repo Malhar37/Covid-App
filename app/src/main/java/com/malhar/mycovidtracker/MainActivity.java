3dea002ccb3e568324e3c55b9afd60941b38a801
Atharv Kulkarni <atharvsk11@gmail.com>
2020-12-03 01:08:46 +0530

    used binding in MainActivity and implemented navigation drawer

--- app/src/main/java/com/malhar/mycovidtracker/MainActivity.java
+++ app/src/main/java/com/malhar/mycovidtracker/MainActivity.java
@@ -2,36 +2,42 @@ package com.malhar.mycovidtracker;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toolbar;
-
-import com.google.android.material.navigation.NavigationView;
+import com.malhar.mycovidtracker.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
-    Toolbar toolbar;
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
+//    private Button button;
+//    Toolbar toolbar;
+//    DrawerLayout drawerLayout;
+//    NavigationView navigationView;
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        toolbar = findViewById(R.id.toolbar);
-        drawerLayout = findViewById(R.id.drawer);
-        navigationView = findViewById(R.id.nav_view);
 
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, "Open Navigation drawer", "Close Navigation drawer");
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, StatisticsActivity.class));
-            }
-        });
+        // binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //setting toolbar
+        setSupportActionBar(binding.toolbar);
+
+        binding.navView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, binding.drawer,
+                binding.toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        binding.drawer.addDrawerListener(toggle);
+
+        //button = findViewById(R.id.button);
+        // toolbar = findViewById(R.id.toolbar);
+        //drawerLayout = findViewById(R.id.drawer);
+        //navigationView = findViewById(R.id.nav_view);
+
+        binding.button.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, StatisticsActivity.class))
+        );
+
     }
 }
